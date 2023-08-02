859b0dce5a3dbfc359f87c517a885dff98dfe6fa
kenethriera <kenethrr@outlook.com>
2023-08-01 19:16:54 -0500

    Avanza con pantalla principal de productos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +9,124 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+  NavigationDestinationLabelBehavior labelBehavior =
+      NavigationDestinationLabelBehavior.alwaysShow;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: labelBehavior,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: <Widget>[
+          NavigationDestination(
+            selectedIcon: SvgPicture.asset('assets/shopping-bag_selected.svg',
+                width: 24, height: 24),
+            icon: SvgPicture.asset('assets/shopping-bag.svg',
+                width: 24, height: 24),
+            label: 'Productos',
+          ),
+          NavigationDestination(
+            selectedIcon: SvgPicture.asset('assets/user_selected.svg',
+                width: 24, height: 24),
+            icon: SvgPicture.asset('assets/user.svg', width: 24, height: 24),
+            label: 'Mi perfil',
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.fromLTRB(6.0, 3.0, 6.0, 3.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: SizedBox(
+                  height: 150,
+                  child: Card(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Image.network(
+                          "https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG",
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'The Enchanted Nightingale',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                              const Padding(
+                                padding:
+                                    EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                                child: Text(
+                                  'Music by Julie Gable. Lyrics by Sidney Stein.',
+                                  style: TextStyle(fontSize: 16),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        const SizedBox(width: 8),
+                                        const Text(
+                                          "Precio: \$87.00",
+                                          style: TextStyle(
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {
+                                            /* ... */
+                                          },
+                                          icon: const Icon(
+                                              Icons.add_shopping_cart),
+                                        ),
+                                        const SizedBox(width: 8),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
